bde42831767a4b7293c15a508f0eb890b9243a18
baeggoose <bsj154@gmail.com>
2023-05-01 23:32:00 +0900

    PHP-MySQL연결

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 	<title>Hello goorm</title>
 </head>
 <body>
-	<h1>Hello baeggoose SQL!!</h1>
-	<?php
-		echo "Contents here";
-	?>
+	<h1>Hello goorm</h1>
+
+    <?php
+		$conn = mysqli_connect("localhost", "root", "123456", "testdb");
+		
+		$sql = "SELECT * FROM testtable";
+		$result = mysqli_query($conn, $sql);
+	
+		while($row = mysqli_fetch_array($result)) {
+		  echo "<li>{$row['id']} - {$row['title']}</li>";
+		}
+		
+		?>
+  
 </body>
 </html>
