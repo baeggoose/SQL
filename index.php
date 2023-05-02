7e3cbef799f9652bd24d8b2570c205dfd3b81ce9
baeggoose <bsj154@gmail.com>
2023-05-02 14:16:29 +0900

    PHP-MySQL Bootstrap CSS stylesheet 적용

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>Hello goorm</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 </head>
 <body>
 	<h1>Hello goorm</h1>
