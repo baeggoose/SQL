e4c0cc7369056c9ca2b9277a6c24f0c7afa08fb7
baeggoose <bsj154@gmail.com>
2023-05-02 14:38:46 +0900

    PHP-MySQL 검색창으로 넘어온 값을 검색 . 문자열 연결

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <?php
 		$conn = mysqli_connect("localhost", "root", "123456", "testdb");
 		
-		// 	와일드카드 중 하나인 %를 사용하여 is인 키워드를 찾는 코드
-		$sql = "SELECT * FROM testtable where description like '%is%'";
+		// 	검색창으로 넘어온 값을 검색하는 코드 각 문자열을 연결시킬 때 + 가 아닌 . 을 사용
+		$sql = "select * from testtable where description like '%".$_GET["search"]."%'";
 		$result = mysqli_query($conn, $sql);
 
 		// bootstrap table
